c4e85c7d13ec753ffe8bafb308818a792db46ce4
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-18 12:20:24 +0530

    Navbar customize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Spring from 'react-spring';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, NavLink, useHistory} from "react-router-dom";
+
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import CVPage from "./components/CVPage";
 import ProjectPage from "./components/ProjectPage";
 import ContactPage from "./components/ContactPage";
+
 import Footer from "./components/Footer";
 import Background from "./components/Background";
 
@@ -16,19 +18,21 @@ function App() {
     lastName: "S G",
     copyright: "Copyright 2020"
   }
+  
   return (
     <div className="container-app">
       <Background/>
       <Router>
+
         <div className="navbar">
-          <Link to="/components/HomePage">Home</Link>
-          <Link to="/components/AboutPage">About</Link>
-          <Link to="/components/CVPage">CV</Link>
-          <Link to="/components/ProjectPage">Projects</Link>
-          <Link to="/components/ContactPage">Contact Me</Link>
+          <NavLink to="/" activeClassName="active-option">Home</NavLink>
+          <NavLink to="/components/AboutPage" activeClassName="active-option">About</NavLink>
+          <NavLink to="/components/CVPage" activeClassName="active-option">CV</NavLink>
+          <NavLink to="/components/ProjectPage" activeClassName="active-option">Projects</NavLink>
+          <NavLink to="/components/ContactPage" activeClassName="active-option">Contact Me</NavLink>
 
           <Switch>
-            <Route exact path = "/components/HomePage">
+            <Route exact path = "/">
               <HomePage data={data}/>
             </Route>
             <Route exact path = "/components/AboutPage">
