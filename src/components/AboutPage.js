f6def309ea1317650b405646296ead9c43c8c466
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-24 18:37:23 +0530

    About Page completed

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import '../index.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBriefcase} from '@fortawesome/free-solid-svg-icons';
 import {faGraduationCap} from '@fortawesome/free-solid-svg-icons';
+import {faAngleUp} from '@fortawesome/free-solid-svg-icons';
 
 export class AboutPage extends Component {
     render() {
@@ -20,32 +22,36 @@ export class AboutPage extends Component {
                 </p>
                 </div>
 
-                <VerticalTimeline style={{padding:'3rem'}}>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{ background: '#6b605a', color: '#fff' , alignContent: 'left'}}
-                  contentArrowStyle={{ borderRight: '7px solid  #e26120' }}
-                  date="August 2020 - present"
-                  iconStyle={{ background: '#6b605a', color: '#fff' }}
-                  icon={<FontAwesomeIcon icon={faBriefcase} style={{fontSize:"50px", marginLeft:"-1.5rem"}}/>}
-                >
-                  <h3 className="vertical-timeline-element-title">Undergraduate Research Assistant</h3>
-                  <h4 className="vertical-timeline-element-subtitle">Solarillion Foundation</h4>
-                  <p>
-                    Research in Machine Learning
-                  </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--education"
-                  date="2018-present"
-                  contentStyle={{ background: '#6b605a', color: '#fff' , alignContent: 'left'}}
-                  contentArrowStyle={{ borderRight: '7px solid  #e26120' }}
-                  iconStyle={{ background: '#6b605a', color: '#fff' }}
-                  icon={<FontAwesomeIcon icon={faGraduationCap} style={{fontSize:"50px", marginLeft:'-1.9rem'}}/>}
-                >
-                  <h3 className="vertical-timeline-element-title">B.E Computer Science and Engineering</h3>
-                  <h4 className="vertical-timeline-element-subtitle">SSN College of Engineering, Chennai</h4>
-                </VerticalTimelineElement>
+                <VerticalTimeline className="vertical-timeline.vertical-timeline-custom-line">
+                  <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#6b605a', color: '#fff' , width: '35%', marginLeft: '8rem', boxShadow:"0px 4px #e26120"}}
+                    contentArrowStyle={{ borderRight: '7px solid  #e26120' }}
+                    date="August 2020 - present"
+                    iconStyle={{ background: '#6b605a', color: '#fff' }}
+                    icon={<FontAwesomeIcon icon={faBriefcase} style={{fontSize: '50px', marginLeft:'-1.5rem'}}/>}
+                  >
+                    <h3 className="vertical-timeline-element-title">Undergraduate Research Assistant</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Solarillion Foundation</h4>
+                    <p>
+                      Research in Machine Learning
+                    </p>
+                  </VerticalTimelineElement>
+                  <VerticalTimelineElement
+                    className="vertical-timeline-element--education"
+                    date="2018-present"
+                    contentStyle={{ background: '#6b605a', color: '#fff' , width: '35%', marginRight: '8rem', boxShadow:"0px 4px #e26120"}}
+                    contentArrowStyle={{ borderRight: '7px solid  #e26120' }}
+                    iconStyle={{ background: '#6b605a', color: '#fff'}}
+                    icon={<FontAwesomeIcon icon={faGraduationCap} style={{fontSize: '50px', marginLeft:'-1.9rem'}}/>}
+                  >
+                    <h3 className="vertical-timeline-element-title">B.E Computer Science and Engineering</h3>
+                    <h4 className="vertical-timeline-element-subtitle">SSN College of Engineering, Chennai</h4>
+                  </VerticalTimelineElement>
+                  <VerticalTimelineElement
+                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    icon={<FontAwesomeIcon icon={faAngleUp} style={{fontSize:"70px", marginLeft:'-1.3rem'}}/>}
+                  />
                 </VerticalTimeline>
             </div>
         )
