3b01bcf1b157c9f867293c2e49f618372950a8e0
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-24 13:15:49 +0530

    Home Page almost complete

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,32 +1,61 @@
 import React, { Component } from 'react';
-import Picture from "./Picture";
+import TextTransition, { presets } from "react-text-transition";
+
+import ProPic from "./images/Profile.jpg";
 import linkedin from "./images/linkedin.ico";
 import github from "./images/github.ico";
 import instagram from "./images/instagram.ico";
 
 export class HomePage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            index:0
+        };
+    }
+    incIndex = () => {
+        this.setState({index: this.state.index + 1});
+    }
+
     render() {
         const {
-            firstname,
-            lastname
+            firstName,
+            lastName,
         } = this.props.data;
+        const TEXTS = [
+            "Data Science",
+            "Machine Learning",
+            "Natural Language Processing"
+          ];
+        setInterval(this.incIndex, 3000);
         return (
             <div className="homepage-container">
-                
-                <div className="name-pane" >
-                <h1>{firstname} {lastname}</h1>
-                    <p>
-                        <a href="https://www.linkedin.com/in/shivanirudh-s-g-b47265199/" target="blank"><img src={linkedin} alt="shivanirudh" height="30" width="30"/></a>
+                <div className="row">
+                    <div className="pic-pane">
+                        <img  className="picture" src={ProPic} alt="ProfilePicture" height="300px" width="300px"/>
+                    </div>
+                    <div className="name-pane" >
+                        <h1>{firstName} {lastName}</h1>
+                        <p> Programmer | Software Developer</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <p className="media-icons">
+                        <a href="https://www.linkedin.com/in/shivanirudh-s-g-b47265199/" target="blank"><img src={linkedin} alt="shivanirudh" height="40" width="40"/></a>
                         &nbsp;&nbsp;
-                        <a href="https://github.com/Shivanirudh" target="blank"><img src={github} alt="shivanirudh" height="30" width="30"/></a>
+                        <a href="https://github.com/Shivanirudh" target="blank"><img src={github} alt="shivanirudh" height="40" width="40"/></a>
                         &nbsp;&nbsp;
-                        <a href="https://instagram.com/shiv_anirudh" target="blank"><img src={instagram} alt="shiv_anirudh" height="30" width="30"/></a>
+                        <a href="https://instagram.com/shiv_anirudh" target="blank"><img src={instagram} alt="shiv_anirudh" height="40" width="40"/></a>
                     </p>
+                    <div className="interests-pane">
+                        <h1>
+                        {console.log(TEXTS)}
+                            <TextTransition
+                                text={ TEXTS[this.index % TEXTS.length] }
+                            />
+                        </h1>
+                    </div>
                 </div>
-                <div className="pic-pane">
-                    <Picture/>
-                </div>
-                <div className="interests"></div>
             </div>
         )
     }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,27 +7,49 @@ import {faUser} from '@fortawesome/free-solid-svg-icons';
 import {faFile} from '@fortawesome/free-solid-svg-icons';
 import {faArchive} from '@fortawesome/free-solid-svg-icons';
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons';
 
 export class Navigation extends Component {
   render() {
     return (
       <div className="sidebar">
         <ul className="sidebar-links">
+          <li className="menu-icon">
+            <Link className="sidebar-item" to="#">
+              <span className="sidebar-item-link menu-icon-text">Map</span>
+              <FontAwesomeIcon className="logo-icon" icon={faAngleDoubleRight} style={{fontSize:"40px"}} rotation={180}/>
+            </Link>
+          </li>
+          <li className="link">
           <Link className="sidebar-item" to="/">
-            <FontAwesomeIcon icon={faHome} style={{fontSize:"40px", paddingRight:"5px"}}/>Home
+            <FontAwesomeIcon icon={faHome} style={{fontSize:"40px"}}/>
+            <span className="sidebar-item-link">Home</span>
           </Link>
+          </li>
+          <li className="link">
           <Link className="sidebar-item" to="/about">
-            <FontAwesomeIcon icon={faUser} style={{fontSize:"40px", paddingRight:"5px"}}/>About
+            <FontAwesomeIcon icon={faUser} style={{fontSize:"40px"}}/>
+            <span className="sidebar-item-link">About</span>
           </Link>
+          </li>
+          <li className="link">
           <Link className="sidebar-item" to="/cv">
-            <FontAwesomeIcon icon={faFile} style={{fontSize:"40px", paddingRight:"5px"}}/>CV
+            <FontAwesomeIcon icon={faFile} style={{fontSize:"40px"}}/>
+            <span className="sidebar-item-link">CV</span>
           </Link>
+          </li>
+          <li className="link">
           <Link className="sidebar-item" to="/projects">
-            <FontAwesomeIcon icon={faArchive} style={{fontSize:"40px", paddingRight:"5px"}}/>Projects
+            <FontAwesomeIcon icon={faArchive} style={{fontSize:"40px", paddingRight:"5px"}}/>
+            <span className="sidebar-item-link">Projects</span>
           </Link>
+          </li>
+          <li className="link">
           <Link className="sidebar-item" to="/contact">
-            <FontAwesomeIcon icon={faEnvelope} style={{fontSize:"40px", paddingRight:"5px"}}/>Contact Me
+            <FontAwesomeIcon icon={faEnvelope} style={{fontSize:"40px"}}/>
+            <span className="sidebar-item-link">Contact Me</span>
           </Link>
+          </li>
         </ul>
         
       </div>
