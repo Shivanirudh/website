d1c0bb415517f2cbdf5415c7e232e68cd2061f2b
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-25 23:28:03 +0530

    Hosted

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -18,15 +18,18 @@ export class ContactPage extends Component {
                         <FontAwesomeIcon className="arrow-icon" icon={faChevronRight}/>
                     </div>
                     <div className="phone-box">
-                        <FontAwesomeIcon className="icon" icon={faEnvelope}/>
-                        <span className="icon-text"><a href="mailto:shivanirudh2001@gmail.com">shivanirudh2001@gmail.com</a></span>
+                        <FontAwesomeIcon className="icon" icon={faPhone}/>
+                        <span className="icon-text">(+91)9445435407</span>
                         <FontAwesomeIcon className="arrow-icon" icon={faChevronRight}/>
                     </div>
-                    <div className="mail-box">
-                        <FontAwesomeIcon className="icon" icon={faEnvelope}/>
-                        <span className="icon-text"><a href="mailto:shivanirudh2001@gmail.com">shivanirudh2001@gmail.com</a></span>
+                    <div className="location-box">
+                        <FontAwesomeIcon className="icon" icon={faMapMarkedAlt}/>
+                        <span className="icon-text"><a href="https://goo.gl/maps/yfM34PP1Ki33DshK8">Chennai, India</a></span>
                         <FontAwesomeIcon className="arrow-icon" icon={faChevronRight}/>
                     </div>
+                </div>
+                <div className="contact-last">
+                    <p>&copy; Copyright 2020</p>
                 </div>
             </div>
         )
